6a3b5af2cb184f4095d36242240c1d1545f02228
manuel_perez <manel.aguila.1606@gmail.com>
2025-07-28 17:03:00 -0600

    corrigiendo error en precio_mes.py

--- precio_mes.py
+++ precio_mes.py
@@ -14,20 +14,66 @@ boletas = [{"fecha_compra" : "29-05-22",
             "productos" : {"Mantequilla": 2, "Azucar" : 1}
             }]
 
+boletas2 = [{'fecha_compra': '11-06-22', 'precio': 15000, 'productos': {'Chocolate': 4}}, {'fecha_compra': '25-06-22', 'precio': 18000, 'productos': {'Leche': 4, 'Huevos': 3}}, {'fecha_compra': '12-05-22', 'precio': 19000, 'productos': {'Leche': 5, 'Pan': 3, 'Chocolate': 4}}, {'fecha_compra': '23-06-22', 'precio': 18000, 'productos': {'Pan': 6, 'Chocolate': 3, 'Mantequilla': 2}}, {'fecha_compra': '24-05-22', 'precio': 15000, 'productos': {'Leche': 2, 'Huevos': 2, 'Azucar': 3, 'Pan': 5}}]
+
 def precio_mes(boletas):
-    precio_mes = dict()
-    total_mes = 0
+
+    precio_fecha = dict()
+
     for boleta in boletas:
-        precio_mes[boleta["fecha_compra"][3:]] = 0        
-        for elem in precio_mes:
-            if elem == boleta["fecha_compra"][3:]:
-                total_mes = total_mes + boleta["precio"]
-                precio_mes[elem] = total_mes
-            elif elem != boleta["fecha_compra"][3:]:
-                total_mes = 0
+        precio_fecha[boleta["fecha_compra"]] = boleta["precio"] 
+    
+    fecha = list(precio_fecha)
+    meses = []
+    for elem in fecha:
+        meses.append(elem[3:])
+
+    meses = set(meses)
+    
+    total_mensual = []
+
+    for elem in precio_fecha.items():
+        total_mensual.append((elem[0][3:],elem[1]))
+
+    total_mensual.sort()
+    mensualidad = 0
+    precio_mes = dict()
+    for mes in meses:
+        mensualidad = 0
+        for elem in total_mensual:
+            if elem[0] == mes:
+                mensualidad = mensualidad + elem[1]
+                precio_mes[elem[0]] = mensualidad
+
+    print(total_mensual)
+
+
     return precio_mes
 
-print(precio_mes(boletas))
+def precio_mes2(boletas):
+    precio_fecha = dict()
+    mensualidad = 0
+
+    for boleta in boletas:
+        precio_fecha[boleta["fecha_compra"]] = boleta["precio"]
+
+    precio_mes = list()
+
+    for t in precio_fecha.items():
+        precio_mes.append((t[0][3:],t[1]))
+
+    precio_mes.sort()
+
+    cuenta_mes = dict()
+
+    for elem in precio_fecha.items():
+
+
+    return precio_mes
+
+
+
+print(precio_mes2(boletas2))
 
 
     
