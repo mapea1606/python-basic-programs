9d12d56a36cd2f11918cdd8a9db244ad85035f61
manuel_perez <manel.aguila.1606@gmail.com>
2025-09-08 20:07:39 -0600

    Tercer intento por aplanar la lista falló, pero hay avance.

--- en_orden.py
+++ en_orden.py
@@ -4,7 +4,7 @@ lista2 = []
 
 def en_orden(lista, ordenada):
     ordenada.append(lista)
-    if len(lista) == 1 and type(lista) == list:
+    if type(lista) == list:
         ordenada.append(lista[0])
         return ordenada
     return en_orden(lista[0], ordenada)
