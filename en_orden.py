285c5e7f4cd10015b800346a32bcc55adc836084
manuel_perez <manel.aguila.1606@gmail.com>
2025-09-09 07:42:48 -0600

    tercer intento

--- en_orden.py
+++ en_orden.py
@@ -9,7 +9,9 @@ def en_orden(lista, ordenada):
         return ordenada
     return en_orden(lista[0], ordenada)
 
+
 ordenada = []
 en_orden(lista, ordenada)
+print(ordenada)
 for x in ordenada:
     print(x)
