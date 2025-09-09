f1521b75338d7d52827c1b8905ae08de55f72700
manuel_perez <manel.aguila.1606@gmail.com>
2025-09-08 19:39:10 -0600

    Primer intento de crear la función aplana listas.

--- en_orden.py
+++ en_orden.py
@@ -1,6 +1,14 @@
 lista = [['a'], [['b'], 'c']]
 
+lista2 = []
+
+def en_orden(lista, ordenada):
+
+    ordenada.append(lista)
+    return ordenada
+
 ordenada = []
 en_orden(lista, ordenada)
-for x in ordenada:
-    print(x)
+print(ordenada)
+#for x in ordenada:
+#    print(x)
