7ed7bcf78d71a28c72b83b47583cf923d0966866
manuel_perez <manel.aguila.1606@gmail.com>
2025-09-08 20:03:38 -0600

    Segundo intento fallido por aplanar la lista.

--- en_orden.py
+++ en_orden.py
@@ -3,12 +3,13 @@ lista = [['a'], [['b'], 'c']]
 lista2 = []
 
 def en_orden(lista, ordenada):
-
     ordenada.append(lista)
-    return ordenada
+    if len(lista) == 1 and type(lista) == list:
+        ordenada.append(lista[0])
+        return ordenada
+    return en_orden(lista[0], ordenada)
 
 ordenada = []
 en_orden(lista, ordenada)
-print(ordenada)
-#for x in ordenada:
-#    print(x)
+for x in ordenada:
+    print(x)
