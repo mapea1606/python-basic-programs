471065f7e3256378a8b39f969644ce06fe877b19
manuel_perez <manel.aguila.1606@gmail.com>
2025-09-10 19:42:42 -0600

    nuevo intento de creación de la funcion aplanadora en el script en_orden.py

--- en_orden.py
+++ en_orden.py
@@ -5,12 +5,11 @@ lista2 = []
 def en_orden(lista, ordenada):
     ordenada.append(lista)
     if type(lista) == list:
-        primero = lista[0]
         resto = lista[1:]
         ordenada.append(primero)
         return ordenada
-    return primero + en_orden(resto, ordenada)
-
+    plana = primero +  en_orden(resto, ordenada)
+    return plana
 
 ordenada = []
 en_orden(lista, ordenada)
