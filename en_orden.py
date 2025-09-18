fdfc10287e9fee87bbce8cd3093c033316c02b06
manuel_perez <manel.aguila.1606@gmail.com>
2025-09-17 20:09:02 -0600

    otro intento fallido

--- en_orden.py
+++ en_orden.py
@@ -4,15 +4,16 @@ lista2 = []
 
 def en_orden(lista, ordenada):
     ordenada.append(lista)
-    if type(lista) == list:
-        resto = lista[1:]
-        ordenada.append(primero)
+    if type(lista[0]) == list:
+        ordenada.append(lista[0])
         return ordenada
-    plana = primero +  en_orden(resto, ordenada)
-    return plana
+    else:
+        ordenada = []
+        return en_orden(lista[1:], ordenada)
+
 
 ordenada = []
 en_orden(lista, ordenada)
 print(ordenada)
-for x in ordenada:
-    print(x)
+#for x in ordenada:
+#    print(x)
