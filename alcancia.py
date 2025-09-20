df81777c5705a8616f5a262750e611cfb15fe57a
manuel_perez <manel.aguila.1606@gmail.com>
2025-09-20 03:58:43 -0600

    Se ha concluido el script alcancia.py, listo para revisión y documentación

--- alcancia.py
+++ alcancia.py
@@ -3,3 +3,14 @@ class Alcancia:
         self.nombre = nombre
         self.dinero = dinero
         self.siguiente_alcancia = alcancia
+
+def sacar_dinero(alcancia, monto):
+    alcancia.dinero = alcancia.dinero - monto
+    if alcancia.dinero >= 0:
+        return (alcancia.nombre, alcancia.dinero)
+    return sacar_dinero(alcancia.siguiente_alcancia, abs(alcancia.dinero))
+
+a1 = Alcancia("Chanchito de greda", 900, None)
+a2 = Alcancia("Debajo del Colchón", 100, a1)
+
+print(sacar_dinero(a2, 2000))
