a8ac76b50f17025d3ec96edcab065322cd29a0e6
manuel_perez <manel.aguila.1606@gmail.com>
2025-09-22 01:55:37 -0600

    Se ha corregido la funcion sacar_dinero del script alcancia.py

--- alcancia.py
+++ alcancia.py
@@ -5,12 +5,20 @@ class Alcancia:
         self.siguiente_alcancia = alcancia
 
 def sacar_dinero(alcancia, monto):
-    alcancia.dinero = alcancia.dinero - monto
-    if alcancia.dinero >= 0:
+    nuevo_monto = alcancia.dinero - monto
+    if alcancia.dinero >= monto:
+        alcancia.dinero = nuevo_monto
+        print(f"{alcancia.nombre}:{alcancia.dinero}")
         return (alcancia.nombre, alcancia.dinero)
-    return sacar_dinero(alcancia.siguiente_alcancia, abs(alcancia.dinero))
+    alcancia.dinero = 0
+    print(f"{alcancia.nombre}:{alcancia.dinero}")
+    return sacar_dinero(alcancia.siguiente_alcancia, abs(nuevo_monto))
 
-a1 = Alcancia("Chanchito de greda", 900, None)
-a2 = Alcancia("Debajo del Colchón", 100, a1)
+a1 = Alcancia("Alcancia a1", 10000, None)
+a2 = Alcancia("Alcancia a2", 700, a1)
+a3 = Alcancia("Alcancia a3", 100, a2)
+a4 = Alcancia("Alcancia a4", 500, a3)
+a5 = Alcancia("Alcancia a5", 900, a4)
+a6 = Alcancia("Alcancia a6", 800, a5)
 
-print(sacar_dinero(a2, 2000))
+print(sacar_dinero(a6, 5900))
