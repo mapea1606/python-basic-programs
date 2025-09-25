56edd3b6d9fea2047160692a106dcbadee4a738f
manuel_perez <manel.aguila.1606@gmail.com>
2025-09-25 13:11:39 -0600

    Probando algoritmo para llenar la sucesión componente del vector.

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,5 +1,26 @@
-charco = True
-print("Comienza la caminata!")
-if charco == True:
-    print("A saltar!")
-print("Fin de la caminata")
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    X = list()
+    i = 0
+    while i <= x:
+        X.append(i)
+        i += 1
+
+    Y = list()
+    j = 0
+    while j <= y:
+        Y.append(j)
+        j += 1
+
+    Z = list()
+    k = 0
+    while k <= z:
+        Z.append(k)
+        k += 1
+
+    print(f"{X},{Y},{Z}")
+    
